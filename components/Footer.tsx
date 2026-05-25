cce864cd5f8ab0addead031b7839cf006fa4cb67
Saaduddin Shaikh <Saaduddin.shaikh-02@legrand-ext.com>
2026-05-25 15:50:54 +0530

    added another footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
 		<footer className="bg-white text-black py-6 mt-8">
 			<div className="max-w-7xl mx-auto px-4 sm:px-8 text-center">
 				<p className="text-lg">Thanks for visit, come again</p>
+                <p className="text-sm text-gray-500 mt-2">© 2024 MyApp. All rights reserved.</p>
 			</div>
 		</footer>
 	);
